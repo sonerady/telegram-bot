2fc37783a64a240d44447414e94fe97e17c98b48
Soner <80179760+sonerady@users.noreply.github.com>
2023-10-06 09:56:15 +0300

    fixed

---
+++ index.js
@@ -0,0 +1,40 @@
+const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const token = "6398218428:AAH-3gYd7AF1zSIOJ7WxZcEl2NmXX7KiEF0";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(
+    chatId,
+    "*Let's get started 🤖*\n\nHow about a glimpse into the AI realm? Discover how it will revolutionize your life!",
+    {
+      parse_mode: "Markdown",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Try it now!",
+              web_app: {
+                url: "https://telegram-k56byvvqc-soners-projects.vercel.app/",
+                show: true,
+              },
+            },
+          ],
+        ],
+      },
+    }
+  );
+});
+
+app.get("/", (req, res) => {
+  res.send("Bot is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
